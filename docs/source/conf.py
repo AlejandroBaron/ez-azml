665c0c5fa97f0cdaaa424c9b5648c1a5bee028d8
Alejandro <alejandrobarongarcia10@gmail.com>
2024-08-10 16:45:07 +0200

    hotfix rdt requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,8 +1,11 @@
 # Configuration file for the Sphinx documentation builder.
 import os
+import re
 import sys
+from pathlib import Path
 
-sys.path.insert(0, os.path.abspath("../../src"))  # src code dir relative to this file
+src_path = os.path.abspath("../../src")
+sys.path.insert(0, src_path)  # src code dir relative to this file
 
 # -- Project information
 project = "Easy AzureML"
@@ -10,12 +13,16 @@ copyright = "2024, Alejandro barón"  # noqa A001
 author = "Alejandro Barón"
 
 release = "0.1"
+version = "0.1.0"
 try:
     from ez_azml.__version__ import __version__
 
     version = __version__
 except ImportError:
-    version = "0.1.0"
+    version_path = Path(src_path) / "ez_azml" / "__version__.py"
+    result = re.search('"([^"]+)"', version_path.read_text())
+    if result:
+        version = result.group(1)
 
 extensions = [
     "sphinx.ext.duration",
